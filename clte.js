b97c2fc5a943848048fe88758cdaa794861e19d6
pa001024 <pa001024@qq.com>
2017-04-14 02:25:39 +0800

    update

--- clte.js
+++
@@ -1,9 +0,0 @@
-
-import { CloudMusic } from './lib/addon/CloudMusic.js';
-
-console.log(CloudMusic.getSecData([28578755],"5feee63aa3b894d7db1f0ec0b5067ce1"));
-
-CloudMusic.getURL([28578755])
-    .then(data => {
-        console.log(data);
-    });
