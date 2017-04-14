1ea8b14325d0155d0f574f6c17d9822126ae308a
pa001024 <pa001024@qq.com>
2017-04-14 22:54:57 +0800

    update RE

---
+++ config.exmple.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "小咸鱼",
+    // 在 https://discordapp.com/developers/applications/me 下可以获取
+    clientId: "2975*********784",
+    token: "Mjk3NTg1NzIw******************2YZGKOelm4c-18",
+
+    prefix: {
+        // 全部信息的前缀 一般不使用
+        main: "",
+        // TTS功能的前缀
+        tts: "",
+        // 指令前缀
+        cmd: "--",
+        // 称呼
+        chat: ["小咸鱼", "咸鱼", "fish"],
+    },
+
+    // 正常对话时的回复率 当加上称呼时无视本选项
+    replyRate: 0.2,
+    
+    chat_api: "http://api.d****************************TkE4QUFBPT0&msg=<MSG>",
+};
