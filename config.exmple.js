83a8c1de22edc8632ea36db5086c3f7d8ecbca9b
pa001024 <pa001024@qq.com>
2018-03-28 19:31:41 +0800

    u

--- config.exmple.js
+++ config.exmple.js
@@ -3,6 +3,8 @@ module.exports = {
     // 在 https://discordapp.com/developers/applications/me 下可以获取
     clientId: "2975*********784",
     token: "Mjk3NTg1NzIw******************2YZGKOelm4c-18",
+	activityType: "LISTENING", // PLAYING,STREAMING,LISTENING,WATCHING
+	activityName: "你说话 | --help",
 
     prefix: {
         // 全部信息的前缀 一般不使用
@@ -17,6 +19,11 @@ module.exports = {
 
     // 正常对话时的回复率 当加上称呼时无视本选项
     replyRate: 0.2,
+    
+	// 自动删除生效的指令的时间(需要"管理信息"权限) 0为不删除
+	deleteOriginCommandDelay: 0,
+	// 自动删除回复的时间 0为不删除
+    deleteReplyDelay: 0,
     
     chat_api: "http://api.d****************************TkE4QUFBPT0&msg=<MSG>",
 };
